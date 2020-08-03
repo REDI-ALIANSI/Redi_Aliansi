e5fc0839ba672159c700306c55cc777ad7851ee3
Muhamad Febrian Alfachri <febrian.alfachri@hotmail.com>
2020-08-03 17:26:33 +0700

    Add LiveTables

---
+++ Application/SMS/BACKUP_PROCEDURES/Command/BackupLiveTables.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Model;
+using MediatR;
+
+namespace Application.SMS.BACKUP_PROCEDURES.Command
+{
+    public class BackupLiveTables : IRequest<Result>
+    {
+        public string ProcedureSmsout { get; set; }
+        public string ProcedureSmsin { get; set; }
+        public string Conn { get; set; }
+    }
+}
---
+++ Application/SMS/BACKUP_PROCEDURES/Command/BackupLiveTablesHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.SMS.BACKUP_PROCEDURES.Command
+{
+    public class BackupLiveTablesHandler : IRequestHandler<BackupLiveTables, Result>
+    {
+        private readonly IPostgreConnection _postgreConnection;
+
+        public BackupLiveTablesHandler(IPostgreConnection postgreConnection)
+        {
+            _postgreConnection = postgreConnection;
+        }
+
+        public async Task<Result> Handle(BackupLiveTables request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                //Call/RUN Procedure SMSOUT
+                string queryCallSmsout = "CALL " + request.ProcedureSmsout + "();";
+                await _postgreConnection.NonQuery(queryCallSmsout, request.Conn);
+
+                //Call/RUN Procedure SMSIN
+                string queryCallSmsin = "CALL " + request.ProcedureSmsin + "();";
+                await _postgreConnection.NonQuery(queryCallSmsin, request.Conn);
+
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                var errorIEnurable = new List<string>() { ex.Message };
+                var resultExceltiop = Result.Failure(errorIEnurable);
+                return resultExceltiop;
+            }
+        }
+    }
+}
